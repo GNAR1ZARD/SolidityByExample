41f4dd52cb4ab7fdb45761d0d2c58ec44b518006
GNAR1ZARD <Ryanamendala@gmail.com>
2024-02-01 06:50:53 -0800

    uploaded more examples

---
+++ examples/36_import/external.sol
@@ -0,0 +1,11 @@
+// You can also import from GitHub by simply copying the url
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// https://github.com/owner/repo/blob/branch/path/to/Contract.sol
+import "https://github.com/owner/repo/blob/branch/path/to/Contract.sol";
+
+// Example import ECDSA.sol from openzeppelin-contract repo, release-v4.5 branch
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/cryptography/ECDSA.sol
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/cryptography/ECDSA.sol";
